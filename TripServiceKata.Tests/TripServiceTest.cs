74e94193b9c3fc825538c89697c17641715e5794
Marc Nazarian <marc.nazarian@gmail.com>
2014-06-28 23:58:07 +0200

    First test: validate that user user is logged in

--- TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata.Tests/TripServiceTest.cs
@@ -1,10 +1,29 @@
-﻿using NUnit.Framework.Constraints;
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+using TripServiceKata.Trip;
+using TripServiceKata.User;
 
 namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
         
-        
+        [Test]
+        [ExpectedException("TripServiceKata.Exception.UserNotLoggedInException")]
+        public void it_should_validate_the_user_is_logged_in()
+        {
+            TripServiceForTests tripService = new TripServiceForTests();
+            tripService.GetTripsByUser(null);
+        }
+
+        private class TripServiceForTests : TripService
+        {
+            protected override User.User GetLoggedUser()
+            {
+                return null;
+            }
+        }
     }
+
 }
