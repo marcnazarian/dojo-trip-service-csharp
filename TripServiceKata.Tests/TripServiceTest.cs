673c9806fa41cd6ee8079518fd84581f271cca55
Marc Nazarian <marc.nazarian@gmail.com>
2014-06-29 14:43:40 +0200

    Refactored TripServiceTest

--- TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata.Tests/TripServiceTest.cs
@@ -8,20 +8,27 @@ namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
+        public const User.User GUEST = null;
+        public readonly User.User A_USER = new User.User();
+        
+        private static User.User loggedInUser;
         
         [Test]
         [ExpectedException("TripServiceKata.Exception.UserNotLoggedInException")]
         public void it_should_validate_the_user_is_logged_in()
         {
             TripServiceForTests tripService = new TripServiceForTests();
-            tripService.GetTripsByUser(null);
+
+            loggedInUser = GUEST;
+
+            tripService.GetTripsByUser(A_USER);
         }
 
         private class TripServiceForTests : TripService
         {
             protected override User.User GetLoggedUser()
             {
-                return null;
+                return loggedInUser;
             }
         }
     }
